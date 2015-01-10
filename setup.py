0d5a2deacc57b66d6adfe5a63934762b53e7d26f
Lee Hyeonseop <protos37@gmail.com>
2015-01-10 22:52:40 +0900

    setup.py and module pwnbox

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "pwnbox",
+    version = "0.1",
+    packages = find_packages(),
+    author = "protos37",
+    author_email = "protos37@gmail.com",
+    url = "http://github.com/protos37/pwnbox"
+)
