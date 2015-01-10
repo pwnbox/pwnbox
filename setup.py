023889051623156339b664f65dadca34a235aae3
Lee Hyeonseop <protos37@gmail.com>
2015-01-11 06:23:13 +0900

    added utility tests

--- setup.py
+++ setup.py
@@ -1,9 +1,10 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
 setup(
     name = "pwnbox",
     version = "0.1",
-    packages = find_packages(),
+    packages = ["pwnbox"],
+    test_suite = "tests",
     author = "protos37",
     author_email = "protos37@gmail.com",
     url = "http://github.com/protos37/pwnbox"
---
+++ tests/__init__.py
@@ -0,0 +1,6 @@
+import unittest
+
+from utils import TestTos
+
+if __name__ == "__main__":
+    unittest.main()
