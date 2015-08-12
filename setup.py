1c92c5f64c0355ccf82625bc91d1875f7afe01d4
Lee Hyeonseop <protos37@gmail.com>
2015-08-12 12:49:52 +0900

    v0.2.1 released: #3 patched

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name = "pwnbox",
-    version = "0.2",
+    version = "0.2.1",
     packages = ["pwnbox"],
     test_suite = "tests",
     author = "protos37",
