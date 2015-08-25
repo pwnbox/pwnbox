454978de26538708256149c214e37680e8f82835
Lee Hyeonseop <protos37@gmail.com>
2015-08-26 06:40:50 +0900

    use nose loader rather than unittest loader

--- setup.py
+++ setup.py
@@ -4,7 +4,8 @@ setup(
     name = "pwnbox",
     version = "0.2.1",
     packages = ["pwnbox"],
-    test_suite = "tests",
+    test_suite = "nose.collector",
+    tests_require=['nose'],
     author = "protos37",
     author_email = "protos37@gmail.com",
     url = "http://github.com/protos37/pwnbox"
