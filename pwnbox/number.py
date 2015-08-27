77dc69406ffdb7e1384c66acd4da847f43f2d9dd
lbh0307 <lbh0307@kaist.ac.kr>
2015-08-27 15:20:38 +0900

    crt, wiener's attack, fermat factoring is added

--- pwnbox/number.py
+++ pwnbox/number.py
@@ -1,1 +1,74 @@
 import gmpy2
+
+def crt(remainders, moduli, coprime = True):
+    assert(len(remainders) == len(moduli))
+    if not coprime:
+        v, m = remainders[0], moduli[0]
+        for u, n in zip(remainders, moduli)[1:]:
+            g,s,t = gmpy2.gcdext(m,n)
+            assert(v%g == u%g)
+            v += s*m/g*(u-v)
+            m*= n/g
+        return (v%m,m)
+
+    p = reduce(lambda x,y : x*y,moduli)
+    v = 0
+    for u, m in zip(remainders, moduli):
+        e = p / m
+        g, s, t = gmpy2.gcdext(e, m)
+        v += e*(u*s % m)
+    return (v%p,p)
+
+def q2cf(n, m):
+    """
+    convert rational number n/m to continued fraction.
+    """
+    res = []
+    while m:
+        x = gmpy2.f_div(n, m)
+        res.append(x)
+        n, m = m, n-m*x
+    return res
+
+def cf_convergents(cf):
+    """
+    convert continued fraction to convergents.
+    """
+    p_2, q_2 = gmpy2.mpz(0), gmpy2.mpz(1)
+    p_1, q_1 = gmpy2.mpz(1), gmpy2.mpz(0)
+    res = []
+    for x in cf:
+        p, q = x*p_1 + p_2, x*q_1 + q_2
+        p_2,q_2 = p_1,q_1
+        p_1,q_1 = p,q
+        res.append((p,q))
+    return res
+
+def wiener_attack(N,e):
+    convergents = cf_convergents(q2cf(e,N))
+    for k,d in convergents:
+        if k==0 or (e*d-1)%k != 0:
+            continue
+        phi = (e*d-1)/k
+        c = N - phi + 1
+        # now p,q can be the root of x**2 - s*x + n = 0
+        det = c*c - 4*N
+        if not det >= 0:
+            continue
+        s, r = gmpy2.isqrt_rem(det)
+        if r==0 and gmpy2.is_even(c+s):
+            return (d,(c+s)/2,(c-s)/2)
+    # Failed
+    return None
+
+def fermat_factoring(N,trial=1<<32):
+    x = gmpy2.isqrt(N)+1
+    y = x*x - N
+    for i in xrange(trial):
+        if gmpy2.is_square(y):
+            y = gmpy2.isqrt(y)
+            return (x+y,x-y)
+        y += (x<<1)+1
+        x += 1
+    # Failed
+    return None
--- pwnbox/tests/test_number.py
+++ pwnbox/tests/test_number.py
@@ -1,7 +1,9 @@
 import unittest
 import imp
 import pwnbox
-
+import random
+import gmpy2
+from Crypto.Util.number import getPrime
 class TestImport(unittest.TestCase):
     def test_has_gmpy2(self):
         has_gmpy2 = False
@@ -18,3 +20,52 @@ class TestImport(unittest.TestCase):
         else:
             with self.assertRaises(AttributeError):
                 pwnbox.number
+
+class TestNumber(unittest.TestCase):
+    def test_crt_coprime(self):
+        v = random.randint(10,10000000000)
+        n = random.randint(1,100)
+        remainders = []
+        moduli = []
+        lcm = 1
+        for i in range(n):
+            while True:
+                mod = random.randint(2,100000000)
+                g = gmpy2.gcd(mod,lcm)
+                if g == 1 : break
+            remainders.append(v%mod)
+            moduli.append(mod)
+            lcm*=mod
+        res = pwnbox.number.crt(remainders,moduli)
+        self.assertEqual(res,(v%lcm,lcm))
+
+    def test_crt_without_coprime(self):
+        v = random.randint(10,10000000000)
+        n = random.randint(1,100)
+        remainders = []
+        moduli = []
+        for i in range(n):
+            mod = random.randint(2,100000000)
+            remainders.append(v%mod)
+            moduli.append(mod)
+        lcm = reduce(gmpy2.lcm,moduli)
+        res = pwnbox.number.crt(remainders,moduli,False)
+        self.assertEqual(res,(v%lcm,lcm))
+
+    def test_fermat_factoring(self):
+        q = getPrime(512)
+        p = gmpy2.next_prime(q + random.randint(1<<64,1<<128))
+        self.assertEqual((p,q),pwnbox.number.fermat_factoring(p*q))
+
+    def test_wiener(self):
+        q = gmpy2.mpz(getPrime(512))
+        p = gmpy2.mpz(getPrime(512))
+        if q>p :
+            p,q = q,p
+        n = p*q
+        phi = (p-1)*(q-1)
+        d = gmpy2.iroot(n,5)[0]
+        while gmpy2.gcd(d,phi) != 1 :
+            d -= 1
+        e = gmpy2.invert(d,phi)
+        self.assertEqual((d,p,q),pwnbox.number.wiener_attack(n,e))
