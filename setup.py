16aa32bc0b120943f3d67c55403ce56916002f31
lbh0307 <lbh0307@kaist.ac.kr>
2015-09-01 00:09:26 +0900

    Now test_number skip tests if gmpy2 not exists

--- setup.py
+++ setup.py
@@ -5,7 +5,8 @@ setup(
     version = "0.2.1",
     packages = ["pwnbox"],
     test_suite = "nose.collector",
-    tests_require=['nose'],
+    tests_require = ["nose", "pycrypto"],
+    install_requires = ["pycrypto"],
     author = "protos37",
     author_email = "protos37@gmail.com",
     url = "http://github.com/protos37/pwnbox"
