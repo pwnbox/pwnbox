6225aa67041f438a84c773c62336adc074887698
Hyeonseop Lee <protos37@gmail.com>
2016-03-11 19:32:24 -0800

    Use find_packages() in setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name = "pwnbox",
     version = "0.4",
-    packages = ["pwnbox"],
+    packages = find_packages(),
     test_suite = "nose.collector",
     tests_require = ["nose", "pycrypto"],
     author = "Hyeonseop Lee",
