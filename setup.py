e3a53cbf4ba8f19717359f7e7ff0e97c46938585
Hyeonseop Lee <hyeonseop@ls-al.me>
2016-05-27 06:20:28 +0900

    Bumped version number to 0.4.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "pwnbox",
-    version = "0.4",
+    version = "0.4.1",
     packages = find_packages(),
     install_requires = ['six'],
     test_suite = "nose.collector",
